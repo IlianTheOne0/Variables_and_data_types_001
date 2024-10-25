e9f20b6b185787012298be554c58b53931f7322d
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-10-25 13:07:12 +0300

    Completed Task 2

---
+++ Task 2.py
@@ -0,0 +1,5 @@
+a = int(input('Salary per month: '))
+b = int(input('The amount of the monthly payment for a bank loan: '))
+c = int(input('Payment for utility services: '))
+
+print('Remains:', a-b-c)
